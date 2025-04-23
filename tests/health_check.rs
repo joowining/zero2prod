965a1b5829c277740bcf4917dbe1d0a4c756e0da
JoowonKim <99549623+joowining@users.noreply.github.com>
2025-04-23 19:08:20 +0900

    오타수정 / 첫번째 통합테스트 작성
    -오타 수정
    zerro2prod -> zero2prod
    
    -첫번째 통합 테스트 작성
    reqwest crate를 추가
    health_check_works() 테스트 함수
    spawn_app() 백그라운드 서버 실행 함수
    작성

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,31 @@
 //! tests/health_check.rs
+//! test/health_check.rs
 
-use zero2prod::main;
+// tokio::test는 test용 tokio::main이 된다.
+// #[test]를 지정할 수고를 덜어준다. 
 
-#[test]
-fn dummy_test(){
-    main()
+// cargo expand --test health_check을 통해 
+// 위의 매크로가 생성하는 코드를 확인할 수 있다. 
+use reqwest;
+
+#[tokio::test]
+async fn health_check_works(){
+	// 준비
+	spawn_app().await.expect("Failed to spawn our app.");
+	// reqwest 를 통해서 클라이언트를 생성하고 어플리케이션에 대한 HTTP요청 시도
+	let client = reqwest::Client::new();
+	
+	let response = client
+		.get("http://127.0.0.1:8000/health_check")
+		.send()
+		.await
+		.expect("Failed to execute request.");
+		
+	assert!(response.status().is_success());
+	assert_eq!(Some(0), response.content_length());
+}
+
+// 백그라운드에서 어플리케이션을 실행
+async fn spawn_app() -> std::io::Result<()>{
+	todo!()
 }
