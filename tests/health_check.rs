e06935cd9c571c65823ff49d64e59cff40875e74
JoowonKim <99549623+joowining@users.noreply.github.com>
2025-04-25 00:49:40 +0900

    chapter 3-6 첫번째 통합 테스트 코드 완성 - 백그라운드 서버 실행 - 동적으로 포트를 제공 - 런타임시 제공 포트 설정

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,16 +7,17 @@
 // cargo expand --test health_check을 통해 
 // 위의 매크로가 생성하는 코드를 확인할 수 있다. 
 use reqwest;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works(){
 	// 준비
-	spawn_app().await.expect("Failed to spawn our app.");
+    let address = spawn_app();
 	// reqwest 를 통해서 클라이언트를 생성하고 어플리케이션에 대한 HTTP요청 시도
 	let client = reqwest::Client::new();
 	
 	let response = client
-		.get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check",&address))
 		.send()
 		.await
 		.expect("Failed to execute request.");
@@ -26,6 +27,18 @@ async fn health_check_works(){
 }
 
 // 백그라운드에서 어플리케이션을 실행
-async fn spawn_app() -> std::io::Result<()>{
-	todo!()
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.1:0")
+            .expect("Failed to bind random port");
+    
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Faild to bind address");
+    let _ = tokio::spawn(server);
+
+
+    format!("http://127.0.1:{}",port)
+    // 서버를 백그라운드로 구동
+    // tokio::spawn은 생성된 퓨처에 대한 핸들을 반환한다.
+    // 하지만 이 퓨처를 다루지 않으므로 일단 무시한다.
+    
 }
