ed0ac77995face72c9e546886186ce401841a44d
JoowonKim <99549623+joowining@users.noreply.github.com>
2025-04-29 09:54:43 +0900

    3.9 신규 구독자 추가하기 데이터베이스 연결을 다루는 Pool을 앱 상태로 추가 핸들러에서 이를 다루도록 추가 구독시의 첫 쿼리문 작성

--- src/startup.rs
+++ src/startup.rs
@@ -2,16 +2,23 @@
 use actix_web::{web, App, HttpServer };
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgPool;
 
 use crate::routes::*;
 // `run`을 pub로 마크해주고 
 // `run`은 바이너리 엔트리 포인트가 아니기에 proc-macro 없이 
 // 단순 async로 마크
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(||{
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool
+    ) -> Result<Server, std::io::Error> {
+
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move ||{
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
