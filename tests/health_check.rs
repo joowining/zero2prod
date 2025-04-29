2d232e8de124bc5077dce11c1c1c9c73af0de6c5
JoowonKim <99549623+joowining@users.noreply.github.com>
2025-04-29 22:10:14 +0900

    테스트 고립성을 고려하지 않은 200 테스트 작성 데이터베이스의 쿼리 실행 및 사후 처리에 대한 부분이 부재

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,8 +8,13 @@
 // 위의 매크로가 생성하는 코드를 확인할 수 있다. 
 use reqwest::{self, Client};
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
+use sqlx::{PgConnection, PgPool, Connection};
 use zero2prod::configuration::get_configuration;
+
+pub struct TestApp {
+    pub address: String,
+    pub connection: PgPool
+}
 
 #[tokio::test]
 async fn health_check_works(){
@@ -29,16 +34,29 @@ async fn health_check_works(){
 }
 
 // 백그라운드에서 어플리케이션을 실행
-fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0")
             .expect("Failed to bind random port");
     
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::startup::run(listener).expect("Faild to bind address");
+    let address = format!("http://127.0.0.1:{}",port);
+
+    let configuration = get_configuration.expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    )
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let server = zero2prod::startup::run(listener, connection_pool.clone()).expect("Faild to bind address");
     let _ = tokio::spawn(server);
 
+    TestApp {
+        address,
+        connection: connection_pool,
+    }
 
-    format!("http://127.0.0.1:{}",port)
+
     // 서버를 백그라운드로 구동
     // tokio::spawn은 생성된 퓨처에 대한 핸들을 반환한다.
     // 하지만 이 퓨처를 다루지 않으므로 일단 무시한다.
@@ -48,21 +66,17 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arange
-    let app_address = spawn_app();
+    let app = spawn_app().await;
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
     // Connection 트레이트는 반드시 스코프 안에 있어야 PgConnection::connect를 호출할 수 있다. 
     // 구조체의 상속 메서드가 아니다. 
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres.");
 
     let client = reqwest::Client::new();
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -73,7 +87,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
     
